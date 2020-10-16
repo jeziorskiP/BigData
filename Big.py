d88347dfcfa65a47c508ecb4ec769dc760a240e2
jeziorskiP <pawel.jeziorski.97@gmail.com>
2020-10-16 15:12:34 +0200

    #Option2 -> Load all data

--- Big.py
+++ Big.py
@@ -9,9 +9,12 @@ from sklearn.datasets import load_iris
 
 fields = ['Complaint Type', 'Borough', 'Agency Name']
 Userchunksize = 100000
-
-#df = pd.read_csv("data.csv", usecols=['Agency Name', 'Complaint Type', 'Borough'])
-
+"""
+Option1 -> wczytwyanie po Userchunksize 
+Option2 -> wczytwyanie całości
+"""
+"""
+#option1
 print("---------------Load---------------------")
 start_time_insert = time.time()
 for chunk in pd.read_csv("data.csv", chunksize=Userchunksize, usecols=fields):
@@ -19,8 +22,18 @@ for chunk in pd.read_csv("data.csv", chunksize=Userchunksize, usecols=fields):
 print("--- %s seconds ---" % (time.time() - start_time_insert))
 print("---------------Load-END-----------------")
 print("")
+"""
 
+#Option2
+print("---------------Load---------------------")
+start_time_insert2 = time.time()
+df = pd.read_csv("data.csv", usecols=fields)
+print("--- %s seconds ---" % (time.time() - start_time_insert2))
+print("---------------Load-END-----------------")
+print("")
 
+"""
+#Option1
 #1
 print("---------P1--Complaint Type---------------------")
 start_time_process1 = time.time()
@@ -48,3 +61,34 @@ last = chunk.groupby('Borough')['Complaint Type'].value_counts()
 print("--- %s seconds ---" % (time.time() - start_time_process3))
 print(last)
 print("----------!Complaint Type per Borough!------------------")
+"""
+
+#Option2
+#1
+print("---------P1--Complaint Type---------------------")
+start_time_process1 = time.time()
+onlyComplaimentType = df['Complaint Type']
+onlyComplaimentType.value_counts()
+
+print("--- %s seconds ---" % (time.time() - start_time_process1))
+print(onlyComplaimentType.value_counts())
+print("---------------!Complaint Type!-----------------")
+
+#2
+print("---------P2--Agency Name-------------------------")
+start_time_process2 = time.time()
+onlyAgencyName = df['Agency Name']
+onlyAgencyName.value_counts()
+
+print("--- %s seconds ---" % (time.time() - start_time_process2))
+print(onlyAgencyName.value_counts())
+print("---------------!Agency Name!---------------------")
+
+#3
+print("--------P3--Complaint Type per Borough-----------")
+start_time_process3 = time.time()
+last = df.groupby('Borough')['Complaint Type'].value_counts()
+print("--- %s seconds ---" % (time.time() - start_time_process3))
+print(last)
+print("----------!Complaint Type per Borough!------------------")
+
